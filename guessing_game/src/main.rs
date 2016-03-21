52b9eaba3aeeff15a64779cbb4da32bc026a0cf1
Woodrow Barlow <wjbarlow@ncsu.edu>
2016-03-21 18:28:42 -0400

    further enhancements to guessing game tutorial

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,40 +5,34 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn read_i32() -> i32 {
+    let mut input = String::new();
     loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)
-            .expect("Failed to read line.");
-        let input: i32 = match input.trim().parse() {
-            Ok(n) => n,
-            Err(_) => {
-                println!("Please input a number.");
-                continue;
-            },
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        match input.trim().parse() {
+            Ok(n) => return n,
+            Err(_) => println!("Please input a number."),
         };
-        return input;
     }
 }
 
 fn read_guess(min: i32, max: i32) -> i32 {
-    let mut guess: i32;
     loop {
-        guess = read_i32();
+        let guess = read_i32();
         if guess < min {
             println!("Please input a number that is at least {}.", min);
         } else if guess > max {
             println!("Please input a number that is no larger than {}.", max);
         } else {
-            break;
+            return guess;
         }
     }
-    guess
 }
 
 fn main() {
     println!("Guess the number!");
     let (min, max) = (1, 10);
-    let secret_number = rand::thread_rng().gen_range(min, max+1);
+    let secret_number = rand::thread_rng().gen_range(min, max + 1);
     loop {
         println!("Please input your guess, between {} and {}.", min, max);
         let guess = read_guess(min, max);
